973a3ce9ae50a5ee1be723c87e575cb2ea14beea
Irene Mendoza <mendozagonzalez.irene@gmail.com>
2026-01-16 07:52:53 +0100

    Inicio del proyecto

---
+++ profiles/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True
+    )
