238ea1f681f06a1892fa9e1710cb90ff7a552733
Irene Mendoza <mendozagonzalez.irene@gmail.com>
2026-01-18 13:44:23 +0100

    feat: Add UserProfile, Follow models and admin registration

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from profiles.models import Follow, UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "birth_date"]
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ["follower", "following", "created_at"]
--- profiles/models.py
+++ profiles/models.py
@@ -1,16 +1,49 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        related_name='profile'
-    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     profile_picture = models.ImageField(
-        upload_to='profile_pictures/',
-        blank=True,
-        null=True
+        upload_to="profile_pictures/", blank=True, null=True
     )
+    bio = models.TextField("Biografia", max_length=500, blank=True)
+    birth_date = models.DateField("Fecha de nacimiento", null=True, blank=True)
+    followers = models.ManyToManyField(
+        "self", symmetrical=False, related_name="following", through="Follow"
+    )
+
+    class Meta:
+        verbose_name = "Perfil"
+        verbose_name_plural = "Perfiles"
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        UserProfile,
+        verbose_name="¿Quién sigue?",
+        on_delete=models.CASCADE,
+        related_name="follower_set",
+    )
+    following = models.ForeignKey(
+        UserProfile,
+        verbose_name="¿A quién sigue?",
+        on_delete=models.CASCADE,
+        related_name="following_set",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="¿Desde cuando lo sigue?"
+    )
+
+    class Meta:
+        verbose_name = "Seguidor"
+        verbose_name_plural = "Seguidores"
+        unique_together = ("follower", "following")
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
